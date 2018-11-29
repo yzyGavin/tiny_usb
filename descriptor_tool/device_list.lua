eaf1df49ed44de7dfafb630794ddabd661f70250
xiongyi <xiongyi@wise-tech.com.cn>
2018-11-29 18:22:39 +0800

    Add OTG core param

--- descriptor_tool/device_list.lua
+++ descriptor_tool/device_list.lua
@@ -2,4 +2,6 @@
 return {
     {name = "STM32F0xx",      maxEp = 7,   maxMem = 1024, },
     {name = "STM32F10x_FS",   maxEp = 7,   maxMem = 512,  },
+    {name = "STM32F7xx_FS",   maxEp = 6,   maxMem = 1280, },
+    {name = "STM32F7xx_HS",   maxEp = 9,   maxMem = 4096, },
 }
